a8d509cc5a44288ffa6fc74345353556c0bad0b0
Tanea19 <tanea.19am@gmail.com>
2021-09-29 18:33:14 +0300

    Add comment

--- MyCompany/Controllers/HomeController.cs
+++ MyCompany/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyCompany.Controllers
 {
+    // Home controller
     public class HomeController : Controller
     {
         public IActionResult Index()
